a07fac81a9164a1c82af2cc558092aae327c460c
Sameh Hammad <eng.sameh1995@gmail.com>
2023-06-05 18:05:10 +0300

    flutter tasks day , day2

---
+++ Flutter/day1_task/bin/day1_task.dart
@@ -0,0 +1,5 @@
+import 'package:day1_task/day1_task.dart' as day1_task;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day1_task.calculate()}!');
+}
---
+++ Flutter/day1_task/test/day1_task_test.dart
@@ -0,0 +1,8 @@
+import 'package:day1_task/day1_task.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
